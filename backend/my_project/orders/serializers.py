0045e5a1d5f7a5685e34a99e19dd7662568f54bc
Irawaa <161902890+Irawaa@users.noreply.github.com>
2025-05-05 07:36:44 +0800

    - Adjusted order files

--- backend/my_project/orders/serializers.py
+++ backend/my_project/orders/serializers.py
@@ -6,3 +6,27 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product', write_only=True
+    )
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity', 'order']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'created_at', 'is_checked_out', 'items']
--- backend/my_project/orders/urls.py
+++ backend/my_project/orders/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from .views import CartItemListCreateView, CheckoutView, OrderListView, OrderDetailView, OrderItemListView, CartItemDeleteView, CartItemUpdateView, CartItemClearView, CustomerOrderDetailView
 
 urlpatterns = [
+    # For customers
+    path('cart/', CartItemListCreateView.as_view(), name='cart'),
+    path('cart/<int:pk>/', CartItemDeleteView.as_view(), name='cart-item-delete'),
+    path('cart/<int:pk>/update/', CartItemUpdateView.as_view(), name='cart-item-update'),
+    path('cart/clear/', CartItemClearView.as_view(), name='cart-clear'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('orders/<int:pk>/', CustomerOrderDetailView.as_view(), name='order-detail'),
 
+    # For employees
+     path('orders/', OrderListView.as_view(), name='order-list'),
+     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+     path('orders/<int:order_id>/items/', OrderItemListView.as_view(), name='order-item-list'),
 ]
