d67c7e2b58b3dd0456b03ecd8ae6b68cf48739f8
Irawaa <161902890+Irawaa@users.noreply.github.com>
2025-05-05 07:47:22 +0800

    - Adjust employee api

--- backend/my_project/orders/serializers.py
+++ backend/my_project/orders/serializers.py
@@ -1,21 +1,11 @@
 from rest_framework import serializers
-from .models import CartItem, Cart, Order, OrderItem
+from .models import Order, OrderItem
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-
-class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
-    product_id = serializers.PrimaryKeyRelatedField(
-        queryset=Product.objects.all(), source='product', write_only=True
-    )
-
-    class Meta:
-        model = CartItem
-        fields = ['id', 'product', 'product_id', 'quantity']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
