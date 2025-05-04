d6e9322d275c0cb87cd1772a9b8bb3671cec4bcf
annapatricialindo <lindoannapatricia46@gmail.com>
2025-05-05 07:58:54 +0800

    Update serializers.py

--- backend/my_project/orders/serializers.py
+++ backend/my_project/orders/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+from .models import CartItem, Cart, Order, OrderItem
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source='product', write_only=True
+    )
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'product_id', 'quantity']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
@@ -20,14 +30,3 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'customer', 'created_at', 'is_checked_out', 'items']
-        
-class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
-    product_id = serializers.PrimaryKeyRelatedField(
-        queryset=Product.objects.all(), source='product', write_only=True
-    )
-
-    class Meta:
-        model = CartItem
-        fields = ['id', 'product', 'product_id', 'quantity']
-
