5e100b83f2ee0f4bc38dfb8ba0ce1c614a00d6ef
Hilario, Rence Gabriel <161903019+Hilario0@users.noreply.github.com>
2025-05-05 00:58:38 +0800

    Update serializers.py

--- backend/my_project/orders/serializers.py
+++ backend/my_project/orders/serializers.py
@@ -6,3 +6,17 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity', 'order']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'created_at', 'is_checked_out', 'items']
