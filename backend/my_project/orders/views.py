20f58e8f9d3b493b85c7cdb4a5d8eb3709457c52
Hilario, Rence Gabriel <161903019+Hilario0@users.noreply.github.com>
2025-05-05 00:57:55 +0800

    Update views.py

--- backend/my_project/orders/views.py
+++ backend/my_project/orders/views.py
@@ -7,3 +7,41 @@ from .serializers import CartItemSerializer, OrderSerializer, OrderItemSerialize
 from accounts.permissions import IsCustomer, IsAdminOrEmployee
 
 # Add to cart
+
+
+# List all checkouts
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminOrEmployee]
+
+    def get_queryset(self):
+        # Fetch orders based on the date filter (if provided)
+        queryset = Order.objects.all()
+
+        # Filter by date if provided in query params
+        date_from = self.request.query_params.get('from', None)
+        date_to = self.request.query_params.get('to', None)
+        
+        if date_from:
+            queryset = queryset.filter(created_at__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(created_at__lte=date_to)
+
+        return queryset
+
+# View order details
+class OrderDetailView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminOrEmployee]
+
+
+
+# List order items (details of a checkout)
+class OrderItemListView(generics.ListAPIView):
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAdminOrEmployee]
+
+    def get_queryset(self):
+        order_id = self.kwargs['order_id']
+        return OrderItem.objects.filter(order__id=order_id)
