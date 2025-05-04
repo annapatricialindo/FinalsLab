9b8982dfe17481775f4e27aee675d9580d0d0091
Hilario, Rence Gabriel <161903019+Hilario0@users.noreply.github.com>
2025-05-05 00:56:07 +0800

    Update urls.py

--- backend/my_project/orders/urls.py
+++ backend/my_project/orders/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import CartItemListCreateView, CheckoutView, OrderListView, OrderDetailView, OrderItemListView, CartItemDeleteView, CartItemUpdateView, CartItemClearView, CustomerOrderDetailView
 
 urlpatterns = [
-
+ path('orders/', OrderListView.as_view(), name='order-list'),
+     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+     path('orders/<int:order_id>/items/', OrderItemListView.as_view(), name='order-item-list'),
 ]
